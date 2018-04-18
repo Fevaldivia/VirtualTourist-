42fe975fe01652c9d8604bf5ec44f8dcb1d2346e
Felipe <fe.valdiviaa@gmail.com>
2018-04-17 19:44:54 -0500

    Initialize Project

---
+++ VirtualTourist/Controller/TravelMapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TravelMapViewController.swift
+//  VirtualTourist
+//
+//  Created by Felipe Valdivia on 4/17/18.
+//  Copyright © 2018 Felipe Valdivia. All rights reserved.
+//
+
+import UIKit
+
+class TravelMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
