665b87ee920ded057ed9c7a466a131c28c2cc386
Felipe <fe.valdiviaa@gmail.com>
2018-04-18 21:20:47 -0500

    Add MapView Delegate
    Add map view delegate to map controller + create extension of mapview
    controller to organize map code.

--- VirtualTourist/Controller/TravelMapViewController.swift
+++ VirtualTourist/Controller/TravelMapViewController.swift
@@ -5,21 +5,24 @@
 //  Created by Felipe Valdivia on 4/17/18.
 //  Copyright © 2018 Felipe Valdivia. All rights reserved.
 //
-
 import UIKit
+import MapKit
 
 class TravelMapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        mapView.delegate = self
     }
 
 
 }
+
+extension TravelMapViewController: MKMapViewDelegate {
+    
+}
+
 
