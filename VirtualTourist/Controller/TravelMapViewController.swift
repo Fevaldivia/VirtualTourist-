2c77ea1ae932e3f44f4b1babda4dd55402f225a8
Felipe <fe.valdiviaa@gmail.com>
2018-04-22 15:41:42 -0500

    add mapview function didSelect pin
    add function to recognize when an user tap on a pin and return latitude
    and longitude.

--- VirtualTourist/Controller/TravelMapViewController.swift
+++ VirtualTourist/Controller/TravelMapViewController.swift
@@ -62,7 +62,8 @@ class TravelMapViewController: UIViewController, MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView){
         //TODO: Implement push to the next controller
-       print("I tapped on ones of the pins!")
+        let selectedAnnotation = view.annotation
+        print("this pin latitude: \(selectedAnnotation!.coordinate.latitude), and longitude: \(selectedAnnotation!.coordinate.longitude)")
     }
 
 }
