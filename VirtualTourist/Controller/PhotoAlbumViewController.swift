0e2fc583ff2cd82bc717730da8b6b8fb2eae5f01
Felipe <fe.valdiviaa@gmail.com>
2018-04-21 22:17:11 -0500

    add photo album controller
    add controller file to show photo albums

---
+++ VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Felipe Valdivia on 4/21/18.
+//  Copyright © 2018 Felipe Valdivia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+    
+}
